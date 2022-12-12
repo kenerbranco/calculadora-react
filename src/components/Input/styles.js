1382e1f62a47abbcfe6d1aac0c0c3bc773cc32d3
Kener de Oliveira Branco <115368851+kenerbranco@users.noreply.github.com>
2022-12-12 05:50:51 -0300

    finalizei estilização base e iniciado incremento dos botões da calculadora

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,1 +1,29 @@
 import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        padding: 0 10px;
+        border: 0;
+        background-color: #AAAAFF;
+
+        display: block;
+        direction: rtl;
+        
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
